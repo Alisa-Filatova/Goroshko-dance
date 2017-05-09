1dd7ee9b9f72d092885493dbf6ae13e7bc3a6551
Alisa Filatova <alicefill@bk.ru>
2017-05-09 03:01:44 +0300

    исправления в меню и напрвлениях

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ var close = document.querySelector('.close-btn');
 open.addEventListener('click', function(event) {
      event.preventDefault();
      nav.classList.add('main-nav__items_show');
-     open.classList.remove('main-nav__menu-open');
+     open.classList.add('main-nav__menu-open_hidden');
 });
 
 close.addEventListener('click', function(event) {
     event.preventDefault();
     nav.classList.remove('main-nav__items_show');
-    open.classList.add('main-nav__menu-open');
+    open.classList.remove('main-nav__menu-open_hidden');
 });
 
 
