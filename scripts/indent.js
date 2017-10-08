7261031320673dad27137114cdafa7cf24770e68
Alisa Filatova <alicefill@bk.ru>
2017-10-09 00:37:17 +0300

    мелкая правка

--- scripts/indent.js
+++ scripts/indent.js
@@ -1,11 +1,16 @@
+/**
+ * Добавляет отступы и переносы строк до и после link и script в index.html,
+ * сгенерированный html-webpack-plugin 
+ */
+
 const fs = require('fs');
 const path = require('path');
 
 const htmlPath = path.join(__dirname, '..', 'index.html');
 
-fs.readFile(htmlPath, 'utf8', (error, html) => {
-    if (error) {
-        console.error(error.toString());
+fs.readFile(htmlPath, 'utf8', (readError, html) => {
+    if (readError) {
+        console.error(readError.toString());
         return;
     }
 
@@ -13,9 +18,9 @@ fs.readFile(htmlPath, 'utf8', (error, html) => {
         .replace(/(<script type="text\/javascript" src="\/dist\/js\/index\.js\?[0-9\w]+"><\/script>)/, '\n    $1\n')
         .replace(/(<link href="\/dist\/css\/index.css\?[0-9\w]+" rel="stylesheet">)/, '    $1\n');
 
-    fs.writeFile(htmlPath, content, (err) => {
-        if (err) {
-            console.error(err.toString());
+    fs.writeFile(htmlPath, content, (writeError) => {
+        if (writeError) {
+            console.error(writeError.toString());
             return;
         }
     });
