3c703275f08cbe138f83bfefda24d0f71cb68c37
Alisa Filatova <alicefill@bk.ru>
2017-11-05 13:58:45 +0300

    внесены изменения в webpack.config.js, выполнена пересборка

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,10 @@ module.exports = {
                 use: {
                     loader: 'html-loader',
                     options: {
-                        attrs: ['video:poster'],
+                        attrs: [
+                            'img:src',
+                            'video:poster',
+                        ],
                     },
                 },
             },
