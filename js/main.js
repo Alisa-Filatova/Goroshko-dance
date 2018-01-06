0359aff1a18277d4b0f039d5ff87b7fbb375a4c7
Alisa Filatova <alicefill@bk.ru>
2018-01-07 01:59:12 +0300

    comment modal post form

--- js/main.js
+++ js/main.js
@@ -17,23 +17,23 @@ close.addEventListener('click', function(event) {
 });
 
 
-var popup = document.querySelector('.modal-post-form');
-var overlay = document.querySelector('.modal-overlay');
-var link = document.querySelector('.table__btn');
-
-link.addEventListener('click', function(event) {
-     event.preventDefault();
-     popup.classList.add('modal-post-form_show');
-     overlay.classList.add('modal-overlay_show');
-});
-
-var closeCross = document.querySelector('.close-cross');
-
-closeCross.addEventListener('click', function(event) {
-    event.preventDefault();
-    popup.classList.remove('modal-post-form_show');
-    overlay.classList.remove('modal-overlay_show');
-});
+// var popup = document.querySelector('.modal-post-form');
+// var overlay = document.querySelector('.modal-overlay');
+// var link = document.querySelector('.table__btn');
+//
+// link.addEventListener('click', function(event) {
+//      event.preventDefault();
+//      popup.classList.add('modal-post-form_show');
+//      overlay.classList.add('modal-overlay_show');
+// });
+//
+// var closeCross = document.querySelector('.close-cross');
+//
+// closeCross.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     popup.classList.remove('modal-post-form_show');
+//     overlay.classList.remove('modal-overlay_show');
+// });
 
 function scrollToAnchor(event) {
     event.preventDefault();
